f9f525910d3cae63fa207ed117e2ea2d3ca04864
Gabe <36457852+Unclechamps@users.noreply.github.com>
2018-05-08 14:47:15 -0500

    updated file

--- todo.py
+++ todo.py
@@ -10,7 +10,22 @@ class Todo:
     def __init__(self, list_description, date):
         self.list_description = list_description
         self.date = date
+        self.exit = False
         self.todo_list = []
+
+    def userOptions(self):
+        if self.exit == False:
+            selectedOption = input("Enter the number for option: 1. Add task, 2. View all tasks, 3. Remove task, Q. Quit application: ")
+            if selectedOption == '1':
+                self.add_task()
+            elif selectedOption == '2':
+                self.list_tasks() #only works if I execute the add_task function and then this one.
+            elif selectedOption == '3':
+                self.remove_tasks()
+            elif selectedOption == 'q' or action == 'Q':
+                self.quit()
+            else:
+                print("Not sure what you are asking for!?")
 
     def add_task(self):
         number_of_tasks = int(input("How many tasks do you want to enter? "))
@@ -19,34 +34,25 @@ class Todo:
             new_task = input("Enter new task: ")
             task_and_priority = priority + " priority - " + new_task
             self.todo_list.append(task_and_priority)
-        print(f"A total of {number_of_tasks} task(s) have been added to your list.")
         print(self.todo_list)
+        self.userOptions()
 
     def list_tasks(self):
         print(self.todo_list)
+        self.userOptions()
 
     def remove_tasks(self):
-        task_to_remove = input("Which task do you want to remove? ")
-        self.todo_list.remove(task_to_remove)
+        task_to_remove = int(input("Which task # do you want to remove? "))-1
+        self.todo_list.remove(self.todo_list[task_to_remove])
         print(self.todo_list)
+        self.userOptions()
 
     def quit(self):
-        pass
+        self.exit = True
 
     def __repr__(self):
         return f"{self.list_description} for {self.date}"
 
 new_todo_list = Todo("Gabe's Todo", "5/7/2018")
 
-action = input("Enter the number for option: 1. Add task, 2. View all tasks, 3. Remove task, Q. Quit application: ")
-
-if action == '1':
-    new_todo_list.add_task()
-elif action == '2':
-    new_todo_list.list_tasks() #only works if I execute the add_task function and then this one.
-elif action == '3':
-    new_todo_list.remove_tasks()
-elif action == 'q' or action == 'Q':
-    new_todo_list.quit()
-else:
-    print("Not sure what you are asking for!?")
+new_todo_list.userOptions()
